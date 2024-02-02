8cf9ff1c9b450f20778af64becca475cc5e8057d
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-02-02 14:29:50 +0200

    Decreasse `AbcSize`

--- lib/onlyoffice_tcm_helper/helpers/rspec_helper.rb
+++ lib/onlyoffice_tcm_helper/helpers/rspec_helper.rb
@@ -11,16 +11,23 @@ module OnlyofficeTcmHelper
     # @param [RSpec::Core::Example] example
     # @return [String] line value
     def self.find_failed_line(example)
-      example_path = example.metadata[:absolute_file_path].downcase
-      dirty_line = example.exception.backtrace.find do |line|
-        next unless (line_path = line[/(.+?):(\d+)(|:\d+)/, 1])
-
-        File.expand_path(line_path).casecmp(example_path).zero?
-      end
+      dirty_line = find_dirty_line(example)
       line_number = dirty_line[/:\d*:/].delete(':').to_i
       OnlyofficeFileHelper::FileHelper.read_array_from_file(example.metadata[:absolute_file_path])[line_number - 1]
     rescue StandardError => e
       "Cannot find failed line because of exception: #{e}"
+    end
+
+    # Find dirty line in example
+    # @param [RSpec::Core::Example] example
+    # @return [String] dirty line
+    def self.find_dirty_line(example)
+      example_path = example.metadata[:absolute_file_path].downcase
+      example.exception.backtrace.find do |line|
+        next unless (line_path = line[/(.+?):(\d+)(|:\d+)/, 1])
+
+        File.expand_path(line_path).casecmp(example_path).zero?
+      end
     end
   end
 end

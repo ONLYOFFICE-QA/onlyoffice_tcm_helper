21945d72a7d9b2be1ea7658e1837ffc499772ba8
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-03-01 12:46:37 +0200

    Fix `rubocop-rspec-2.27.0` cop `RSpec/DescribedClass` warnings.

--- spec/onlyoffice_tcm_helper_spec.rb
+++ spec/onlyoffice_tcm_helper_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe OnlyofficeTcmHelper do
   it 'has a version number' do
-    expect(OnlyofficeTcmHelper::VERSION).not_to be_nil
+    expect(described_class::VERSION).not_to be_nil
   end
 end

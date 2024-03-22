61a12f63880acb908b32916cc7bd7d3525c8bfb9
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-03-22 16:48:55 +0200

    Fix stuff

--- spec/onlyoffice_tcm_helper/tcm_helper_comments_spec.rb
+++ spec/onlyoffice_tcm_helper/tcm_helper_comments_spec.rb
@@ -16,7 +16,8 @@ RSpec.describe OnlyofficeTcmHelper::TcmHelper, '#comment' do
 
     it 'check comment for failed status' do
       failure = "Cannot find failed line because of exception: undefined method ['`]backtrace'"
-      expect(tcm_helper.parse(PseudoExampleFailed.new('check comment for passed_2 status')).comment).to match(/#{failure}/)
+      example = PseudoExampleFailed.new('check comment for passed_2 status')
+      expect(tcm_helper.parse(example).comment).to match(/#{failure}/)
     end
 
     it 'check comment for aborted status' do
